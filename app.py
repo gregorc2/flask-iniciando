9fcc86466051e26f9484f79661d7a13a02150a28
Gregory Camargo <c25gregor@gmail.com>
2022-06-08 11:57:27 -0500

    render al html

--- app.py
+++ app.py
@@ -1,10 +1,19 @@
-from flask import Flask
+from flask import Flask, request, make_response, redirect, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
+def index():
+    user_id = request.remote_addr
+    response = make_response(redirect("/hello"))
+    response.set_cookie("user_id", user_id)
+    return response
+
+@app.route("/hello")
 def hello():
-    return "hola"
+    user_id = request.cookies.get("user_id")
+    return render_template("hello.html", user_id = user_id)
 
 
 if __name__ == "__main__":
